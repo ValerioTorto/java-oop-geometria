6a1d16535105d8814fc2b14e052bb22e77daeefc
ValerioTorto <valerio.tortorelli@gmail.com>
2023-12-05 17:04:49 +0100

    2 part

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,12 +1,29 @@
 package org.lessons.java.geometria;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner rectangleSides = new Scanner(System.in);
+        int base;
+        int height;
+        System.out.print("Quanti cm misura la base? ");
+        base = rectangleSides.nextInt();
+        System.out.print("Quanti cm misura l'altezza? ");
+        height = rectangleSides.nextInt();
+
+     //   int perimetro = 2 * (base + height);
+       // }
+       // int area = base * height;
+       // }
+
+
+   /* public static void main(String[] args) {
         Rettangolo rettangolo = new Rettangolo(3, 5);
         String rettangoloString = rettangolo.toString();
 
         System.out.println(rettangolo);
-
+    */
 
 
     }
