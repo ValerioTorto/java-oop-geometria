dbfc5253a45a7b3a465e8de3566ce6aeaf2dc676
ValerioTorto <valerio.tortorelli@gmail.com>
2023-12-05 15:48:53 +0100

    First commit

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -1,4 +1,8 @@
 package org.lessons.java.geometria;
 
 public class Main {
+    public static void main(String[] args) {
+
+
+    }
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -1,5 +1,18 @@
 package org.lessons.java.geometria;
 
 public class Rettangolo {
+    //ATTRIBUTI
+    int base;
+    int height;
+
+    //COSTRUTTORE CON PARAMETRI
+     class Rectangle {
+         Rectangle(int base, int height) {
+             base = 3;
+             height = 5;
+         }
+
+    }
+
 
 }
